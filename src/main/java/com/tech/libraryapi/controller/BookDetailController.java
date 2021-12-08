993c32ae6277b2fd8e9acee618a10cbda154a847
zhouxuh <zhouxuh@users.noreply.github.com>
2021-12-08 00:38:55 -0500

    Created API endpoints updateBook(), deleteBookById(), getBookById(), and getBookByAuthor() in controller class BookDetailController.java.

--- src/main/java/com/tech/libraryapi/controller/BookDetailController.java
+++ src/main/java/com/tech/libraryapi/controller/BookDetailController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.util.List;
 
@@ -37,5 +38,32 @@ public class BookDetailController {
                                        @PathVariable(value="isbn") String isbn) {
         Book book = bookDetailService.getBookByIsbn(isbn);
         return new ResponseEntity<>(book, HttpStatus.OK);
+    }
+
+    @PutMapping("book/{id}")
+    public ResponseEntity<Book> updateBook(@Positive(message = "{book.id.positive}")
+                                           @PathVariable(value="id") Long id,
+                                           @Valid @RequestBody Book book) {
+        Book updatedBook = bookDetailService.updateBook(id, book);
+        return new ResponseEntity<>(updatedBook, HttpStatus.OK);
+    }
+
+    @DeleteMapping("book/{id}")
+    public ResponseEntity<Book> deleteBookById(@Positive(message = "{book.id.positive}")
+                                               @PathVariable(value="id") Long id) {
+        Book deletedBook = bookDetailService.deleteBookById(id);
+        return new ResponseEntity<>(deletedBook, HttpStatus.OK);
+    }
+
+    @GetMapping("book/{id}")
+    ResponseEntity<Book> getBookById(@Positive(message = "{book.id.positive}")
+                                     @PathVariable(value="id") Long id) {
+        return new ResponseEntity<>(bookDetailService.getBookById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("book/author/{author}")
+    ResponseEntity<List<Book>> getBookByAuthor(@Size(min = 1, max = 80, message = "{book.author.size}")
+                                               @PathVariable(value="author") String author) {
+        return new ResponseEntity<>(bookDetailService.getBookByAuthor(author), HttpStatus.OK);
     }
 }

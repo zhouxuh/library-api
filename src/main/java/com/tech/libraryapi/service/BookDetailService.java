141b5a1b7cd8ef90dd0f22c97b07f03a82dcc0fa
zhouxuh <zhouxuh@users.noreply.github.com>
2021-12-08 00:44:30 -0500

    Added methods updateBook(), deleteBookById(), getBookById(), and getBookByAuthor() into service BookDetailServiceImpl.

--- src/main/java/com/tech/libraryapi/service/BookDetailService.java
+++ src/main/java/com/tech/libraryapi/service/BookDetailService.java
@@ -8,4 +8,8 @@ public interface BookDetailService {
     List<Book> getAllBooks();
     Book addBook(Book book);
     Book getBookByIsbn(String isbn);
+    Book updateBook(Long id, Book book);
+    Book deleteBookById(Long id);
+    Book getBookById(Long id);
+    List<Book> getBookByAuthor(String author);
 }
--- src/main/java/com/tech/libraryapi/service/BookDetailServiceImpl.java
+++ src/main/java/com/tech/libraryapi/service/BookDetailServiceImpl.java
@@ -37,4 +37,38 @@ public class BookDetailServiceImpl implements BookDetailService {
     public Book getBookByIsbn(String isbn) throws BookNotFoundException {
         return bookDetailRepository.findBookByIsbn(isbn).orElseThrow(() -> new BookNotFoundException(isbn));
     }
+
+    @Override
+    public Book updateBook(Long id, Book newBook) {
+        return bookDetailRepository.findById(id)
+                .map(book -> {
+                    book.setAuthor(newBook.getAuthor());
+                    book.setIsbn(newBook.getIsbn());
+                    book.setName(newBook.getName());
+                    book.setPrice(newBook.getPrice());
+                    book.setQuantity(newBook.getQuantity());
+                    book.setDate(newBook.getDate());
+                    return bookDetailRepository.save(book);
+                })
+                .orElseThrow(() -> new BookNotFoundException(id));
+    }
+
+    @Override
+    public Book getBookById(Long id) {
+        return bookDetailRepository.findById(id).orElseThrow(() -> new BookNotFoundException(id));
+    }
+
+    @Override
+    public List<Book> getBookByAuthor(String author) {
+        List<Book> bookList = bookDetailRepository.findBookByAuthor(author);
+        if (bookList.isEmpty()) throw new BookNotFoundException(author);
+        return bookList;
+    }
+
+    @Override
+    public Book deleteBookById(Long id) {
+        Book book = bookDetailRepository.findById(id).orElseThrow(() -> new BookNotFoundException(id));
+        bookDetailRepository.deleteById(id);
+        return book;
+    }
 }
